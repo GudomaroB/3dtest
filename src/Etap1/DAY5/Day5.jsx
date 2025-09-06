3995b22209651f84619b24cc5191c46babd33f89
Vasiliy <vasiliy@example.com>
2025-09-06 22:33:53 +0500

    fix: удалил react-scripts и обновил шкаф

--- src/Etap1/DAY5/Day5.jsx
+++ src/Etap1/DAY5/Day5.jsx
@@ -25,23 +25,23 @@ const Camera = () => {
 
   return (
     <>
-      <ambientLight intensity={0.4} />
-      <PerspectiveCamera makeDefault position={[0, 7, 7]} />
+      <ambientLight intensity={0.8} />
+      <PerspectiveCamera makeDefault position={[0, 7, 8]} />
       <OrbitControls makeDefault />
       <directionalLight
         ref={svet}
-        intensity={1.4}
+        intensity={1}
         castShadow
-        position={[1.2, 5, 4]}
+        position={[3, 5, 5]}
         shadow-mapSize={[1024, 1024]}
-        shadow-camera-left={-3.5}
-        shadow-camera-right={3.5}
-        shadow-camera-top={3.5}
-        shadow-camera-bottom={-3.5}
+        shadow-camera-left={-4}
+        shadow-camera-right={4}
+        shadow-camera-top={4}
+        shadow-camera-bottom={-4}
         shadow-camera-near={0.4}
         shadow-camera-far={16}
-        shadow-bias={-0.001}
-        normalBias={0.4}
+        shadow-bias={-0.00001}
+        normalBias={0.03}
       />
     </>
   );
@@ -63,7 +63,7 @@ const Floor = () => {
 //!Шкаф простой
 const Shkaf = () => {
   return (
-    <group castShadow position={[0, 0, 0]}>
+    <group position={[0, 0.0039, 0]}>
       {/* задняя стенка */}
       <mesh castShadow position={[0, 1.04, 0]}>
         <boxGeometry args={[1.5, 2, 0.02]} />
@@ -72,26 +72,42 @@ const Shkaf = () => {
       {/* Левая стенка */}
       <mesh castShadow position={[-0.76, 1.04, 0.5]}>
         <boxGeometry args={[0.02, 2, 1.02]} />
-        <meshStandardMaterial color={"#d1d2d3"} />
+        <meshStandardMaterial color={"#666666"} />
       </mesh>
       {/* Правая стенка */}
-      <mesh castShadow position={[0.76, 1.04, 0.5]}>
+      <mesh metalness={0.3} position={[0.76, 1.04, 0.5]}>
         <boxGeometry args={[0.02, 2, 1.02]} />
-        <meshStandardMaterial color={"#d1d2d3"} />
+        <meshStandardMaterial color={"#666666"} />
       </mesh>
       {/* Крыша */}
       <mesh position={[0, 2 + 0.05, 0.5]}>
         <boxGeometry args={[1.5 + 0.04, 0.02, 1 + 0.02]} />
-        <meshStandardMaterial color={"#fff"} />
+        <meshStandardMaterial color={"#666666"} />
+      </mesh>
+      {/* Дно */}
+      <mesh castShadow position={[0, 0.02, 0.5]}>
+        <boxGeometry args={[1.5 + 0.04, 0.04, 1 + 0.02]} />
+        <meshStandardMaterial color={"#666666"} />
       </mesh>
       {/* Полка в центре */}
-      <mesh receiveShadow position={[0, 1, 0.45]}>
-        <boxGeometry args={[1.5, 0.02, 0.8]} />
-        <meshStandardMaterial color={"#cabcbcff"} />
+      <mesh castShadow position={[0, 1, 0.45]}>
+        <boxGeometry args={[1.5, 0.04, 0.9]} />
+        <meshStandardMaterial color={"#666666"} />
+      </mesh>
+      {/* Полка */}
+      <mesh position={[0, 1.7, 0.3]}>
+        <boxGeometry args={[1.5, 0.02, 0.5]} />
+        <meshStandardMaterial color={"#666666"} />
+      </mesh>
+      {/* Дверь левая  */}
+      <mesh rotation={[0, -Math.PI / 4, 0]} position={[-0.5, 1.02, 1.26]}>
+        <boxGeometry args={[0.7, 2.08, 0.03]} />
+        <meshStandardMaterial color={"#d1d2d3"} />
       </mesh>
     </group>
   );
 };
+//!Шкаф простой
 
 export const Day5 = () => {
   return (
